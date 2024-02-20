9a43daa5e9760dff3b0fded33a74c5d5afe79129
togethercodingArang <ruhunsu3@naver.com>
2024-02-20 15:40:05 +0900

    20240220

--- problems/20240220.js
+++ problems/20240220.js
@@ -1,6 +1,9 @@
 // 최소 직사각형 (완전탐색)
 // https://school.programmers.co.kr/learn/courses/30/lessons/86491?language=javascript
 
+// TIL
+// javascript에서 배열의 최소 / 최대값을 알고싶다면 Math.max(...arr)
+// 스프레드 문법으로 적용시킬 수 있다. - 시간복잡도 O(N)
 function solution(sizes) {
   let max = 0;
   let min = 0;

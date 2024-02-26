651a87b8c274f73924678ec545a8f8035e8fa887
togethercodingArang <ruhunsu3@naver.com>
2024-02-26 13:02:57 +0900

    20240223-1

--- problems/20240223/20240223-1.js
+++ problems/20240223/20240223-1.js
@@ -2,21 +2,51 @@
 // https://school.programmers.co.kr/learn/courses/30/lessons/1844
 // 최단거리는 BFS를 이용해 풀자 (Queue)
 
+// function solution(maps) {
+//   const yLen = maps.length;
+//   const xLen = maps[0].length;
+//   const goalY = yLen - 1;
+//   const goalX = xLen - 1;
+//   const dy = [0, 0, 1, -1];
+//   const dx = [-1, 1, 0, 0];
+
+//   const queue = [];
+//   queue.push([0, 0, 1]);
+
+//   while(queue.length) {
+//       const [curY, curX, move] = queue.shift();
+//       if(curY === goalY && curX === goalX) return move;
+
+//       for(let i = 0; i < 4; i++) {
+//           const ny = curY + dy[i];
+//           const nx = curX + dx[i];
+//           if(ny >= 0 && ny < yLen && nx >= 0 && nx < xLen && maps[ny][nx] === 1) {
+//               queue.push([ny, nx, move + 1]);
+//               maps[ny][nx] = 0;
+//           }
+//       }
+//   }
+
+//   return -1;
+// }
+
 function solution(maps) {
-  let answer = 0;
+  // x, y축 길이
   let xLength = maps[0].length;
   let yLength = maps.length;
+  // 목표지점 좌표
   let goalX = xLength - 1;
   let goalY = yLength - 1;
-  let dx = [1, -1, 0, 0];
-  let dy = [0, 0, 1, -1];
+  // 상,하,좌,우 좌표
+  let dx = [0, 0, 1, -1];
+  let dy = [1, -1, 0, 0];
 
-  const queue = [[0, 0]];
+  const queue = [[0, 0, 1]];
 
   while (queue.length) {
-    let [curX, curY] = queue.shift();
+    let [curY, curX, move] = queue.shift();
     if (curX === goalX && curY === goalY) {
-      return answer;
+      return move;
     }
     for (let i = 0; i < 4; i++) {
       let nextX = curX + dx[i];
@@ -26,11 +56,10 @@ function solution(maps) {
         nextY >= 0 &&
         nextX < xLength &&
         nextY < yLength &&
-        maps[nextX][nextY] === 1
+        maps[nextY][nextX] === 1
       ) {
-        answer++;
-        queue.push([nextX, nextY]);
-        maps[nextX][nextY] = 0;
+        queue.push([nextY, nextX, move + 1]);
+        maps[nextY][nextX] = 0;
       }
     }
   }
@@ -38,10 +67,10 @@ function solution(maps) {
 }
 console.log(
   solution([
-    [1, 0, 1, 1, 1],
-    [1, 0, 1, 0, 1],
-    [1, 0, 1, 1, 1],
-    [1, 1, 1, 0, 1],
-    [0, 0, 0, 0, 1],
+    [1, 1, 1, 1, 1],
+    [1, 0, 0, 0, 1],
+    [1, 1, 1, 1, 1],
+    [1, 0, 0, 0, 0],
+    [1, 1, 1, 1, 1],
   ])
 );

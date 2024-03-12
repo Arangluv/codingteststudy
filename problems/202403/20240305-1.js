d990365f1a3cf954b419b5256857317e3a3119ed
togethercodingArang <ruhunsu3@naver.com>
2024-03-12 16:37:45 +0900

    20240312 다이나믹 공부

--- problems/202403/20240305-1.js
+++ problems/202403/20240305-1.js
@@ -28,3 +28,27 @@ function solution(participant, completion) {
 console.log(
   solution(["mislav", "stanko", "mislav", "ana"], ["stanko", "ana", "mislav"])
 );
+
+// 좋은 정답코드
+// map을 순회할때 for of 를 사용할 수 있고, 그 값을 초기화할때 let [ley, value] 이런식으로 할당가능하다,
+// let a = ...,
+//     b = ...;
+// 이런식으로 코드를 작성하는것도 깔끔하다
+
+// function solution(participant, completion) {
+//     const map = new Map();
+
+//     for(let i = 0; i < participant.length; i++) {
+//         let a = participant[i], 
+//             b = completion[i];
+
+//         map.set(a, (map.get(a) || 0) + 1);
+//         map.set(b, (map.get(b) || 0) - 1);
+//     }
+
+//     for(let [k, v] of map) {
+//         if(v > 0) return k;
+//     }
+
+//     return 'nothing';
+// }

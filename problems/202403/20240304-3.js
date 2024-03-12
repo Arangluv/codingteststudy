7f0b8fd05469770214de2ff913ab7083cef889b1
togethercodingArang <ruhunsu3@naver.com>
2024-03-12 17:56:05 +0900

    20240304-3 다시 풀기

--- problems/202403/20240304-3.js
+++ problems/202403/20240304-3.js
@@ -1,4 +1,21 @@
 // 뒤에 있는 큰 수 찾기 - 프로그래머스 lv2
 // https://school.programmers.co.kr/learn/courses/30/lessons/154539#
-
-function solution() {}
+// 다시풀기
+function solution(numbers) {
+  // ex. numbers = [9 1 5 3 6 7] -> [-1 5 6 6 7 -1]
+  let answer = [];
+  let stack = [numbers[0]];
+  let index = 1;
+  while (stack.length) {
+    let popedNum = stack.pop();
+    if (popedNum < numbers[index]) {
+      answer.push(numbers[index]);
+      stack.push(numbers[index]);
+      index++;
+    } else {
+      stack.push(numbers[index]);
+      index++;
+    }
+  }
+  return answer;
+}
---
+++ problems/202403/dp/onetwothreeplus.js
@@ -0,0 +1,21 @@
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "input.txt";
+let input = fs.readFileSync(filePath).toString().trim().split("\n");
+
+let nArr = input.map((n) => parseInt(n));
+console.log(nArr);
+
+function solution(n) {
+  let dp = Array(11).fill(0);
+  dp[1] = 1;
+  dp[2] = 2;
+  dp[3] = 4;
+  for (let i = 4; i <= 10; i++) {
+    dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3];
+  }
+  n.forEach((item) => {
+    console.log(dp[item]);
+  });
+}
+
+solution(nArr);

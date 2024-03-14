6c7ba0e0647d463452a40dcc4cc4021d65798d53
togethercodingArang <ruhunsu3@naver.com>
2024-03-14 13:02:10 +0900

    20240313-2 다시풀기 완료 -> 문제조건 잘 읽기

--- problems/202403/20240313-2.js
+++ problems/202403/20240313-2.js
@@ -1,15 +1,26 @@
-function solution(people, limit) {
-  let answer = 1;
-  let peopleArr = people.sort((a, b) => a - b);
-  let temp = limit;
-  for (let i = 0; i <= peopleArr.length - 2; i++) {
-    temp -= peopleArr[i];
-    if (temp - peopleArr[i + 1] < 0) {
-      temp = limit;
-      answer++;
-    }
-  }
+// 구명보트 - > 그리디
+// https://school.programmers.co.kr/learn/courses/30/lessons/42885
 
+// 문제 조건이 매우 중요하다
+// [20 20 30 30]인 케이스를 생각해서 다른방법으로 접근했다
+// 최소 40이상 240이하인 조건을 생각하면 두명만 더하는 덧셈만 고려하면된다.
+
+function solution(people, limit) {
+  let answer = 0;
+  let start = 0;
+  let end = people.length - 1;
+  people = people.sort((a, b) => a - b);
+  while (start < end) {
+    let sum = people[start] + people[end];
+    if (sum > limit) {
+      end--;
+    } else {
+      end--;
+      start++;
+    }
+    answer++;
+  }
+  if (start === end) answer++;
   return answer;
 }
-solution([30, 40, 60, 70], 100);
+console.log(solution([20, 20, 30, 30], 100));

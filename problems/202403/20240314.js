c9b88ce9eacb793a365f2a914860c62314cfa220
togethercodingArang <ruhunsu3@naver.com>
2024-03-15 16:02:27 +0900

    20240314 -> 유클리드 호제법 공부 후 다시

--- problems/202403/20240314.js
+++ problems/202403/20240314.js
@@ -1,20 +1,10 @@
 // n개의 최소공배수
 // https://school.programmers.co.kr/learn/courses/30/lessons/12953
-
+// 유클리드 호제법
 function solution(arr) {
-  let answer = 1;
-  arr.sort((a, b) => a - b);
-  let num = [];
-  for (let i = 1; i < arr.length; i++) {
-    num.push(arr[i]);
-    arr.forEach((n, idx) => {
-      if (n % arr[i] === 0) {
-        arr[idx] = n / arr[i];
-      }
-    });
+  let numArr = arr.sort((a, b) => a - b);
+  let maps = new Map();
+  for (let i = 0; i < numArr.length; i++) {
+    for (let j = 1; j <= numArr[i]; j++) {}
   }
-  arr.forEach((n) => {
-    answer *= n;
-  });
-  return answer;
 }

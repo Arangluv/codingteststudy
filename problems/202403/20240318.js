c6317ad90b8da8f0049ae169c4d22412f48ac0ec
togethercodingArang <ruhunsu3@naver.com>
2024-03-18 15:48:40 +0900

    20240318

---
+++ problems/202403/20240318.js
@@ -0,0 +1,13 @@
+// 예상 대진표
+// https://school.programmers.co.kr/learn/courses/30/lessons/12985
+
+function solution(N, A, B) {
+  let round = 0;
+  while (A !== B) {
+    A = Math.ceil(A / 2);
+    B = Math.ceil(B / 2);
+    round++;
+  }
+  return round;
+}
+console.log(solution(8, 1, 2));

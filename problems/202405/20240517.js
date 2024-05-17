bf79ee7ba1cf1228904432922618636e1a6b3fe3
togethercodingArang <ruhunsu3@naver.com>
2024-05-17 16:26:16 +0900

    숫자변환 dp로 해결 -> dp로 해결하는 과정 눈 여겨 볼것

--- problems/202405/20240517.js
+++ problems/202405/20240517.js
@@ -5,31 +5,15 @@ function solution(x, y, n) {
   // 최소 연산횟수 return -> 만들 수 없다면 -1
   // x -> y
   // x + n, x * 2, x * 3
-  let answer = 0;
-  if (x === y) return 0;
-  function dfs(target, count) {
-    if (target > y) {
-      return -1;
-    }
-    if (target === y) {
-      answer = answer === 0 || answer === -1 ? count : Math.min(count, answer);
-      return;
-    }
-
-    for (let i = 0; i <= 2; i++) {
-      if (i === 0) {
-        // case 1. x + n
-        dfs(target + n, count + 1);
-      } else if (i === 1) {
-        // case 2. x * 2
-        dfs(target * 2, count + 1);
-      } else {
-        // case 3. x * 3
-        dfs(target * 3, count + 1);
-      }
-    }
+  let arr = Array(y + 1).fill(Infinity);
+  console.log(arr);
+  arr[x] = 0;
+  for (let i = x; i <= y; i++) {
+    if (i - n >= x) arr[i] = Math.min(arr[i], arr[i - n] + 1);
+    if (i % 2 === 0 && i / 2 >= x) arr[i] = Math.min(arr[i], arr[i / 2] + 1);
+    if (i % 3 === 0 && i / 3 >= x) arr[i] = Math.min(arr[i], arr[i / 3] + 1);
   }
-  dfs(x, 0);
-  return answer === 0 ? -1 : answer;
+  return arr[y] === Infinity ? -1 : arr[y];
 }
 
+console.log(solution(30, 40, 5));

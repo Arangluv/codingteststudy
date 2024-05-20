34cb6268f1a9d1bfe636b33cbaad74f144ebac49
togethercodingArang <ruhunsu3@naver.com>
2024-05-20 16:03:24 +0900

    2개 이하로 다른 비트 solved -> 구현에 초점에 맞추기보다는 어떤식으로 풀어나갈지 오래생각하면 좋을거 같다

--- problems/202405/20240520.js
+++ problems/202405/20240520.js
@@ -2,23 +2,24 @@
 // https://school.programmers.co.kr/learn/courses/30/lessons/77885
 
 function solution(numbers) {
-  let answer = [];
-  numbers.forEach((number) => {
-    let binary = number.toString(2).padStart(17, "0");
-    for (let i = number + 1; ; i++) {
-      let count = 0;
-      let tempBinary = i.toString(2).padStart(17, "0");
-      for (let j = 0; j < 17; j++) {
-        if (tempBinary[j] !== binary[j]) {
-          count++;
-        }
-      }
-      if (count <= 2 && count >= 1) {
-        answer.push(i);
-        break;
-      }
+  return numbers.map((number) => {
+    if (number % 2 === 0) {
+      // 짝수인 경우
+      let binary = number.toString(2).split("");
+      binary[binary.length - 1] = 1;
+      console.log(binary.join(""));
+      return parseInt(binary.join(""), 2);
     }
+    // 홀수인경우
+    let binary = number.toString(2).split("");
+    let lastIdx = binary.lastIndexOf("0");
+    if (lastIdx === -1) {
+      binary.unshift("0");
+    }
+    let index = lastIdx === -1 ? 0 : binary.lastIndexOf("0");
+    binary[index] = 1;
+    binary[index + 1] = 0;
+    return parseInt(binary.join(""), 2);
   });
-  return answer;
 }
 console.log(solution([2, 7]));

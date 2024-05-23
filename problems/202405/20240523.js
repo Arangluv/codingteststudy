9ad82e3cc2dd40b94a3e4e39b7cb487cd2b48bfb
togethercodingArang <ruhunsu3@naver.com>
2024-05-23 17:14:50 +0900

    큰 수 만들기 fail -> 다시생각

---
+++ problems/202405/20240523.js
@@ -0,0 +1,30 @@
+// 큰 수 만들기 - 그리드
+// https://school.programmers.co.kr/learn/courses/30/lessons/42883
+
+function solution(number, k) {
+  // k개 만큼 지워야함
+  let arr = number.split("");
+  let prev = [parseInt(arr[0]), 0]; // [value, idx]
+  for (let i = 1; i < arr.length; i++) {
+    if (k === 0) break;
+    let current = parseInt(arr[i]);
+    if (current === 2) {
+      console.log("여기 실행");
+    }
+    if (prev[0] < current) {
+      let [value, idx] = prev;
+      k--;
+      arr[idx] = "";
+      prev = [current, i];
+    }
+    if (prev[0] > current) {
+      let [v, idx] = prev;
+      arr[i] = "";
+      k--;
+    }
+  }
+
+  return k === 0 ? arr.join("") : arr.slice(0, arr.length - k).join("");
+}
+
+console.log(solution("4177252841", 4)); // 775841
